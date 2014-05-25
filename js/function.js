22c58b0caa29d404755b15fb85c47252af7d1685
DarkCobalt <haker.inf@gmail.com>
2014-05-25 12:36:12 +0200

    Comments replies

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-var Reddit = angular.module('Reddit', ['ngRoute'])
+var Reddit = angular.module('Reddit', ['ngRoute','RecursionHelper'])
 
 
 
@@ -20,9 +20,12 @@ Reddit.config(['$routeProvider', function($routeProvider) {
     	templateUrl: 'views/coments.html', 	
     	controller: 'EntryCtrl'
   	})
-  //$routeProvider.otherwise({redirectTo: '/'})
+  $routeProvider.otherwise({redirectTo: '/'})
 }])
 
+
+var new_comments = [];
+var root = 0;
 
 Reddit.controller('EntryCtrl', function($scope, $http, $routeParams, $reddit) {
   $scope.links = []
@@ -51,17 +54,43 @@ Reddit.controller('EntryCtrl', function($scope, $http, $routeParams, $reddit) {
       url = $routeParams.domain
     }
   }
-	
+
+  
   $http.get(base_url + (url || '') + '/.json').success(function(res, status) {
      $scope.loading = false
      if(comments){
-     	console.log(res[1].data.children)
      	$scope.links = res[0].data.children
         $scope.coments = res[1].data.children
+        
+        angular.forEach($scope.coments, function(value, key) {
+			
+			new_comments.push({'autor' : value.data.author, 'comment':value.data.body, 'root':root});
+
+			if (value.data.replies != "") 
+				searchTree(value.data.replies.data.children);
+
+	    }, new_comments);
+	    $scope.new_comments = new_comments;
      }else{
        $scope.links = res.data.children
      }
-  })
+      
+});
+
+
+function searchTree(currChild){
+	  root++;
+    for(var i=0; i < currChild.length; i ++){
+		new_comments.push({'autor' : currChild[i].data.author, 'comment':currChild[i].data.body,'root':root});
+
+    	if (currChild[i].data.replies != "") {
+    		searchTree(currChild[i].data.replies.data.children);
+    	}
+     }
+     root--;	
+     return null;
+}
+	
 })
 
 
